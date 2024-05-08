7aaafe82817829a0e29ce6062c3ce0f390127794
ben6brewer <123130980+ben6brewer@users.noreply.github.com>
2024-05-08 13:05:01 -0700

    Delete CS460 directory

--- CS460/PA3/populateTables.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=/usr/lib/oracle/19.8/client64/lib/ojdbc8.jar:${CLASSPATH}
-javac PopulateTables.java
-java PopulateTables
--- CS460/PA3/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CLASSPATH=/usr/lib/oracle/19.8/client64/lib/ojdbc8.jar:$CLASSPATH
-javac Prog3.java
-java Prog3
